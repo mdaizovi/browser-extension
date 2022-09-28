67088662b532e18f045cbfc12a0f79ae0d4e4bda
Mic <mdaizovi@gmail.com>
2022-09-28 17:37:10 +0200

    simple iteration of text in dom works

--- popup.js
+++ popup.js
@@ -1,6 +1,57 @@
 // Initialize button
 let blurButton = document.getElementById("blurButton");
 
+function getDomPath(el) {
+  var stack = [];
+  while ( el.parentNode != null ) {
+    console.log(el.nodeName);
+    var sibCount = 0;
+    var sibIndex = 0;
+    for ( var i = 0; i < el.parentNode.childNodes.length; i++ ) {
+      var sib = el.parentNode.childNodes[i];
+      if ( sib.nodeName == el.nodeName ) {
+        if ( sib === el ) {
+          sibIndex = sibCount;
+        }
+        sibCount++;
+      }
+    }
+    if ( el.hasAttribute('id') && el.id != '' ) {
+      stack.unshift(el.nodeName.toLowerCase() + '#' + el.id);
+    } else if ( sibCount > 1 ) {
+      stack.unshift(el.nodeName.toLowerCase() + ':eq(' + sibIndex + ')');
+    } else {
+      stack.unshift(el.nodeName.toLowerCase());
+    }
+    el = el.parentNode;
+  }
+  return stack.slice(1); // removes the html element
+}
+
+function iterateDom() {
+  console.log("iterateDom");
+  const allElements = document.getElementsByTagName('*');
+  const allInBody = document.querySelectorAll('body > *');
+
+  for (const element of allInBody) {
+    //console.log(element);
+    let text = element.textContent;
+    console.log(text);
+  
+    // var path = getDomPath(element);
+    // console.log(path.join(' > '));
+  }
+
+}
+
+
+// https://stackoverflow.com/questions/10596417/is-there-a-way-to-get-element-by-xpath-using-javascript-in-selenium-webdriver
+// or
+//$x("some xpath")
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
 // When the button is clicked, inject setPageBackgroundColor into current page
 blurButton.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -9,11 +60,21 @@ blurButton.addEventListener("click", async () => {
       target: { tabId: tab.id },
       func: blurText,
     });
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: iterateDom,
+    });
+
+
+
   });
   
   // The body of this function will be executed as a content script inside the
   // current page
   function blurText() {
+
+    console.log("Blurring text");
     chrome.storage.sync.get("blurInt", ({ blurInt }) => {
       var blurStyle = `blur(${blurInt}px)`;
       document.body.style.filter = blurStyle;
